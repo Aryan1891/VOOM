e616105f8c305f8b5ad924e0ef5c1c0aa1318bbd
Aryan1891 <aryanjangiroffical@gmail.com>
2024-07-16 03:22:04 +0530

    created responsiv nav bar  and added clerk authentication

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,10 @@
+import { SignIn } from '@clerk/nextjs'
+import React from 'react'
+
+const SignInPage = () => {
+  return (
+    <main><SignIn/></main>
+  )
+}
+
+export default SignInPage
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,27 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
-
+import MobileNav from './MobileNav'
+import {SignedIn,UserButton} from '@clerk/nextjs'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='flex-between fixed  z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
+        <Link href='/' className='flex items-center gap-1'>
+        <Image 
+        src='/icons/logo.svg' alt='logo' width={32} height={32} className='max-sm:size-10 ' />
+        <p className='text-[26px] font-extrabold text-white max-sm:hidden'>
+           Voom 
+        </p>
+        </Link>
+        <div className='flex-between gap-5'>
+        <SignedIn>
+              <UserButton/>
+            </SignedIn>
+
+        <MobileNav />
+
+        </div>
+    </nav>
   )
 }
 
