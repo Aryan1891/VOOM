50dc4cd276e3d32b40b2e260bcddc19bee8064ea
Aryan1891 <aryanjangiroffical@gmail.com>
2024-07-17 02:30:34 +0530

    feat: Add StreamClientProvider and Toaster components
    The code changes include adding the `StreamClientProvider` component for Stream video integration and the `Toaster` component for displaying toast notifications. This commit also includes updates to the `RootLayout` component to include the `Toaster` component.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
-'use client'
+'use client';
 import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -34,7 +35,9 @@ export default function RootLayout({
     >
     <html lang="en">
       
-    <body className={`${inter.className} bg-dark-2`}>{children}</body>
+    <body className={`${inter.className} bg-dark-2`}>{children}
+    <Toaster/> 
+    </body>
     </html>
     </ClerkProvider>
 
--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -1,16 +1,71 @@
  'use client';
- import React, { useState } from 'react'
-import HomeCard from './HomeCard';
+ import { useState } from 'react';
 import { useRouter } from 'next/navigation';
+import HomeCard from './HomeCard';
 import MeetingModal from './MeetingModal';
- 
+import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk';
+import { useUser } from '@clerk/nextjs';
+import Loader from './Loader';
+// import { Textarea } from './ui/textarea';
+// import ReactDatePicker from 'react-datepicker';
+import { useToast } from './ui/use-toast';
+// import { Input } from './ui/input';
+
+const initialValues = {
+  dateTime: new Date(),
+  description: '',
+  link: '',
+};
+
  const MeetingTypeList = () => {
   const router = useRouter();
-  const [meetingState,setMeetingState] = useState<'isScheduleMeeting'|'isJoiningMeeting'| 'isInstantMeeting' | undefined>()
+  const [meetingState, setMeetingState] = useState<
+    'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined
+  >(undefined);
+  const [values, setValues] = useState(initialValues);
+  const [callDetail, setCallDetail] = useState<Call>();
+  const client = useStreamVideoClient();
+  const { user } = useUser();
+  const { toast } = useToast();
 
-  const createMeeting = () => { 
 
-  }
+  const createMeeting = async () => {
+    if (!client || !user) return;
+    try {
+      if (!values.dateTime) {
+        toast({ title: 'Please select a date and time' });
+        return;
+      }
+      const id = crypto.randomUUID();
+      const call = client.call('default', id);
+      if (!call) throw new Error('Failed to create meeting');
+      const startsAt =
+        values.dateTime.toISOString() || new Date(Date.now()).toISOString();
+      const description = values.description || 'Instant Meeting';
+      await call.getOrCreate({
+        data: {
+          starts_at: startsAt,
+          custom: {
+            description,
+          },
+        },
+      });
+      setCallDetail(call);
+      if (!values.description) {
+        router.push(`/meeting/${call.id}`);
+      }
+      toast({
+        title: 'Meeting Created',
+      });
+    } 
+    
+    catch (error) {
+      console.error(error);
+      toast({ title: 'Failed to create Meeting' });
+    }
+  };
+
+
   
    return (
      <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
